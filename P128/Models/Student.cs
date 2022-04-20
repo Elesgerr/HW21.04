74aadf1f833a0d4fd44645545eb6c085da6aa370
Alesger <Alasgarma@code.edu.az>
2022-04-21 01:28:30 +0400

    Task

---
+++ P128/Models/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P128.Models
+{
+    class Student 
+    {
+        //public static string FullName;
+        //public static string GroupNo;
+        //public static byte Age;
+        //public static bool CheckGroupNo()
+        //{
+        //    string value = "";
+        //    if (value.Length >= 3)
+        //    {
+        //        bool checkUpper = false;
+
+        //        foreach (char item in value)
+        //        {
+        //            if (char.IsUpper(item))
+        //            {
+        //                checkUpper = true;
+        //            }
+        //            if (checkUpper)
+        //            {
+        //                return true;
+        //            }
+        //        }
+        //        if (!checkUpper)
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine(" Ilk Herf Boyuk Olmalidir ");
+        //        value = Console.ReadLine();
+        //    }
+        //    return false;
+        //}
+        //public static void CheckFullName()
+        //{
+        //    Console.WriteLine($"Ad Soyad: {FullName}");
+        //}
+    }
+}
---
+++ P128/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using P128.Models;
+namespace P128
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Daxil Et");
+            //Console.ReadLine();
+            //Student student = new Student();
+            //Student.CheckFullName();
+            //Student.CheckGroupNo();
+
+
+            //Student.FullName = " Eli ";
+            //Student.GroupNo = "P128";
+            //Student.Age = 20;
+
+            //Console.WriteLine($"{Student.FullName} {Student.GroupNo} {Student.Age}");
+
+
+        }
+    }
+}
